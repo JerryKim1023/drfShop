a58d7ecc36e58ef0a336376ba2fd2097f9d2fe20
JerryKim <ecec1023@gmail.com>
2022-06-23 16:46:27 +0900

    Add  templates in settings.py
    for this django can find teamplates files

--- drfShop/settings.py
+++ drfShop/settings.py
@@ -73,7 +73,7 @@ ROOT_URLCONF = 'drfShop.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [BASE_DIR / 'templates'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
