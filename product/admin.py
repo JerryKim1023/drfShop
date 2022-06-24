ec529fb6a72bd18bb23eee2fdd83897511b054be
JerryKim <ecec1023@gmail.com>
2022-06-24 22:44:28 +0900

    Add admin
    to see real datas and to figure out how these date have to be fixed
    I made some admin settings and saw at postman real data too.
    SO I know what to fix.

---
+++ product/admin.py
@@ -0,0 +1,41 @@
+from django.contrib import admin
+from product.models import Product as ProductModel
+from product.models import ProductOption as ProductOptionModel
+from product.models import Category as CategoryModel
+
+from django.utils.safestring import mark_safe
+
+# Register your models here.
+class ProductAdmin(admin.ModelAdmin):
+    list_display = (
+        "title", 
+        # "thumbnail",
+        "category",
+        "title",
+        "desc",
+        "created",
+        "modified",
+        "show_expired_date",
+        "stock",
+        "is_active",
+
+        "thumbnail_preview",
+    )
+    def thumbnail_preview(self, obj):
+        return mark_safe(f'<img src="/product/thumbnail/{obj.id}/" height="100px"/>')
+
+    thumbnail_preview.short_decription = "Thumbnail"
+
+class ProductOptionAdmin(admin.ModelAdmin):
+    list_display = (
+        "product", 
+        "name",
+        "quantity",
+        "size",
+        "price"
+    )
+
+
+admin.site.register(ProductModel, ProductAdmin)
+admin.site.register(ProductOptionModel, ProductOptionAdmin)
+admin.site.register(CategoryModel)
