57e819870cc640d24a0aa92366d75bdc8ee6a116
JerryKim <ecec1023@gmail.com>
2022-06-25 22:24:35 +0900

    Add admin which things are about user's choice
    seeing at admin page I'm gonna figure out what's wrong

---
+++ userchoice/admin.py
@@ -0,0 +1,13 @@
+from django.contrib import admin
+
+from .models import Review as ReviewModel
+from .models import Like as LikeModel
+from .models import Cart as CartModel
+
+
+class ReviewAdmin(admin.ModelAdmin):
+    list_display = ('product', 'rating', 'content')
+# Register your models here.
+admin.site.register(LikeModel)
+admin.site.register(ReviewModel, ReviewAdmin)
+admin.site.register(CartModel)
