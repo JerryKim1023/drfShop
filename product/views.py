463f3eb0464784a014fa3d16c4bc7d21ff15adab
JerryKim <ecec1023@gmail.com>
2022-06-29 20:36:27 +0900

    Fix all the templates, to send only response

--- product/views.py
+++ product/views.py
@@ -20,26 +20,31 @@ from product.models import Product as ProductModel
 from product.models import ProductOption as ProductOptionModel
 from product.models import Category as CategoryModel
 
-from product.serializers import CategorySerializer, ProductSerializer
+from product.serializers import ProductSerializer 
+from product.serializers import CategorySerializer
 from userchoice.serializers import CartSerializer
 
 # Create your views here.
 
-# 모든 상품에 대한 처리
+# 상품에 대한 처리
 class ProductItemView(APIView):
     
-    #모든 상품에 대해서 product 정보 가져오고
-    permission_classes = [UserSeller]
-    renderer_classes = [TemplateHTMLRenderer]
-    template_name = 'sell_product.html'
+    #카테고리별 상품에 대해서 product 정보 가져오고
+    # permission_classes = [UserSeller]
+    permission_classes = [permissions.AllowAny] # 포스트맨 요청받기 위해 임시로 
+    # renderer_classes = [TemplateHTMLRenderer]
+    # template_name = 'sell_product.html'
     def get(self, request):
-        category = CategoryModel.objects.all()
-        category_serializer = CartSerializer(category)
-        if not request.user.is_seller or not request.user.is_admin:
-            return Response(template_name = 'index.html', status=status.HTTP_401_UNAUTHORIZED)
-        return Response(category_serializer.data, template_name = 'sell_product.html', status=status.HTTP_200_OK)
+        product = ProductModel.objects.all()
+        category = CategoryModel.objects.all().order_by('?').first() # 임시로 카테고리 랜덤으로 하나 뽑아오게함.
+        
+        product_serializer = ProductSerializer(product, many=True)
 
-        # return data
+        # if not True :
+        #     return Response(status=status.HTTP_401_UNAUTHORIZED) # template_name = 'index.html', 
+        return Response(product_serializer.data, status=status.HTTP_200_OK) #  template_name = 'sell_product.html',
+
+        # return data example
         """
         {
             "username": "user",
@@ -54,8 +59,9 @@ class ProductItemView(APIView):
         '''
         상품 정보를 입력받아 create 하는 함수
         '''
-        product_sale_serializer = ProductSerializer(data=request.data)
-        if product_sale_serializer.is_valid(): 
+        
+        product_sale_serializer = ProductSerializer(data=request.data, partial=True)
+        if product_sale_serializer.is_valid():
             product_sale_serializer.save() # 정상
             return Response(product_sale_serializer.data, status=status.HTTP_200_OK)
 
@@ -83,73 +89,19 @@ class ProductItemView(APIView):
         # 오브젝트, data , partial 넘기기
         return Response({"message": f"{ProductModel.username} 정보가 더 이상 존재하지 않습니다."}, status=status.HTTP_200_OK)
 
-#카테고리별로처리
-# class ProductCategoryView(APIView):
-    
-#     #카테고리별로 상품에 대해서 product 정보 가져오고
-#     permission_classes = [permissions.AllowAny]
-#     def get(self, request , obj_id):
-#         category = CategoryModel.get(obj_id)
-#         serialized_product_data = ProductSerializer(category).data # 오브젝트를 넣어서 직렬화해주기
-#         return Response(serialized_product_data, many=True, status=status.HTTP_200_OK)
 
-#         # return data
-#         """
-#         {
-#             "username": "user",
-#             "password": "pbkdf2_sha256$320000$u5YnmKo9luab9csqWpzRsa$pKfqHnBiF5Rgdo1Mj9nxNOdhpAl9AhPVXFPXkbPz7Mg=",
-#             "fullname": "user's name",
-#             "email": "user@email.com"
-#         }
-#         """
-
-#     # 상품 등록
-#     def post(self, request):
-#         '''
-#         상품 정보를 입력받아 create 하는 함수
-#         '''
-#         product_sale_serializer = ProductSerializer(data=request.data)
-#         if product_sale_serializer.is_valid(): 
-#             product_sale_serializer.save() # 정상
-#             return Response(product_sale_serializer.data, status=status.HTTP_200_OK)
-
-#         return Response(product_sale_serializer.errors, status=status.HTTP_400_BAD_REQUEST)
-      
-#     # 상품 수정
-#     def put(self, request, obj_id):
-        
-#         product_sale_info_change = ProductModel.objects.get(id=obj_id)
-#         # 오브젝트, data , partial 넘기기
-#         product_sale_update = ProductSerializer(product_sale_info_change, data=request.data, partial=True)
-#         product_sale_update.is_valid(raise_exception=True)
-#         product_sale_update.save()
-#         return Response(product_sale_update.data, status=status.HTTP_200_OK)
-        
-#     # 삭제
-#     def delete(self, request, obj_id):
-        
-#         try:
-#             product_delete = ProductModel.objects.get(obj_id)  
-#         except ProductModel.DoesNotExist:
-#          # some event						   status=400
-#             return Response({"message": "오브젝트가 존재하지 않습니다."}, status=status.HTTP_400_BAD_REQUEST)
-#         product_delete.delete()
-#         # 오브젝트, data , partial 넘기기
-#         return Response({"message": f"{ProductModel.username} 정보가 더 이상 존재하지 않습니다."}, status=status.HTTP_200_OK)
-
-# 특정 아이템에 대한 처리
 
 class CategoryView(APIView):
     
     #해당 카테고리에 대한 product 정보 가져오고
     # permission_classes = [UserSeller]
-    renderer_classes = [TemplateHTMLRenderer]
-    template_name = 'sell_product.html'
+    # renderer_classes = [TemplateHTMLRenderer]
+    # template_name = 'sell_product.html'
 
     def get(self, request, obj_id):    # 카테고리 id
         category = CategoryModel.objects.get(obj_id)
         serialized_product_data = ProductSerializer(category).data # 오브젝트를 넣어서 직렬화해주기
-        return Response(serialized_product_data, template_name = 'sell_product.html', status=status.HTTP_200_OK)
+        return Response(serialized_product_data, status=status.HTTP_200_OK) # , template_name = 'sell_product.html'
 
         # return data
         """
