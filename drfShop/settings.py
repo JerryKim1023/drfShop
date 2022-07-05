16af96e79c6a1c65af8b9c0fcf7561179123d95d
JerryKim <ecec1023@gmail.com>
2022-07-05 22:59:40 +0900

    Add some of cors settingw
    when front server contact to backend server, CORS error occured.

--- drfShop/settings.py
+++ drfShop/settings.py
@@ -46,6 +46,8 @@ INSTALLED_APPS = [
     'product',
     'userchoice',
 
+    'corsheaders', # CORS 에러때문에 추가 1
+
 ]
 
 REST_FRAMEWORK = {
@@ -70,6 +72,8 @@ REST_FRAMEWORK = {
 
 
 MIDDLEWARE = [
+    'corsheaders.middleware.CorsMiddleware', # CORS 에러때문에 추가 2
+
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
@@ -78,6 +82,8 @@ MIDDLEWARE = [
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
+
+CORS_ALLOWED_ORIGINS = ["http://127.0.0.1:5000"]
 
 ROOT_URLCONF = 'drfShop.urls'
 
