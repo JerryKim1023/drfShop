de9934da7ab220fe3c193dc09452d9056f596aaf
김 지용 <74017167+JerryKim1023@users.noreply.github.com>
2022-08-10 16:48:55 +0900

    장고 시크릿키 환경변수에 담아서 처리

--- drfShop/settings.py
+++ drfShop/settings.py
@@ -23,7 +23,8 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-!0$$ca$(bsx^ie9=h)345ol7=df*g-k^e=89rbc&++84h4$txi'
+SECRET_KEY = os.environ.get("SECRET_KEY")
+
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
